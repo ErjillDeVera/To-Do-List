9d42ca8f3983a4c119785d5a116975d571a20137
ErjillDeVera <erjilldevera@icloud.com>
2021-08-17 17:18:32 +0100

    Added a port where the app can listen to.

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+})

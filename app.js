b5ac3fd1c20dbb94c1ba6d0e9b85957ab2061293
ErjillDeVera <erjilldevera@icloud.com>
2021-08-19 09:41:23 +0100

    Refactored code to use toLocaleDateString()

--- app.js
+++ app.js
@@ -10,33 +10,14 @@ app.set("view engine", "ejs"); //App uses ejs as its view engine
 app.get("/", function(req, res){
 
   var today = new Date();
-  var day = "";
-
-  switch (today.getDay()) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day= "Friday";
-      break;
-    case 6:
-      day ="Saturday";
-      break;
-    default:
-      console.log("Error: Current day is equal to: " + currentDay);
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
   }
+
+  var day = today.toLocaleDateString("en-US", options);
+
   res.render("list", {day: day});
 
 });

5b64d9ba3c3417de4296fcac6639ced5abca7a20
ErjillDeVera <erjilldevera@icloud.com>
2021-08-19 08:11:05 +0100

    Used res.render to test ejs template.

--- app.js
+++ app.js
@@ -10,12 +10,35 @@ app.use("view engine", "ejs"); //App uses ejs as its view engine
 app.get("/", function(req, res){
 
   var today = new Date();
+  var day = "";
 
-  if(today.getDay() === 6 || today.getDay() === 0) {
-    res.send("Yay its the weekend!");
-  } else {
-    res.send("Boo!");
+  switch (today.getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day= "Friday";
+      break;
+    case 6:
+      day ="Saturday";
+      break;
+    default:
+      console.log("Error: Current day is equal to: " + currentDay);
   }
+  res.render("list", {day: day});
+
 });
 
 app.listen(3000, function() {

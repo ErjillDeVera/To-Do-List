bc12571f683e4da312a223f463197f95880ad074
ErjillDeVera <erjilldevera@icloud.com>
2021-08-20 11:25:06 +0100

    Fixed bug so that it adds to the correct list.

--- app.js
+++ app.js
@@ -30,29 +30,20 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res) {
   let item = req.body.newItem;
+  console.log(req.body);
 
-  if (req.body.list === "Work") {
+  if (req.body.button === "Work") {
     workItems.push(item);
     res.redirect("/work");
   } else {
     items.push(item);
     res.redirect("/");
   }
-
-  items.push(item)
-
-  res.redirect("/");
-})
+});
 
 app.get("/work", function(req, res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems})
-})
-
-app.post("/work", function(req, res){
-  let item = req.body.newIteml
-  workItems.push(item);
-  res.redirect("/work")
-})
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");

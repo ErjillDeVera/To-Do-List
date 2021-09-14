b29b8d252322502d4feba56a900191a25c0020ae
ErjillDeVera <erjilldevera@icloud.com>
2021-09-14 09:03:20 +0100

    Deleted work route to allow users to have many lists.

--- app.js
+++ app.js
@@ -80,10 +80,6 @@ app.post("/delete", function(req,res){
   });
 });
 
-app.get("/work", function(req, res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
-
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
